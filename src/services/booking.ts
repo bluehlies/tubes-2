4fe932a0dfe9f72a83884cfa3d5880c6e12cca23
bluehlies <153311882+bluehlies@users.noreply.github.com>
2023-12-11 12:02:45 +0700

    Update booking.ts

--- src/services/booking.ts
+++ src/services/booking.ts
@@ -1,14 +1,11 @@
-import supabase from "@/config/supabaseClient";
+import { supabase } from "@/config/supabaseClient";
 import { BookingDataTypes } from "./types";
 
 export async function setNewBooking(bookingData: BookingDataTypes) {
   let { data, error } = await supabase.rpc("newbooking", {
     ...bookingData,
   });
-
-  if (error) {
-    throw new Error("failed create new booking");
-  }
+  
 
   return data;
 }
